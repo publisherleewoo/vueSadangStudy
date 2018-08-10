ae0a921a182518a05faf3c8864488b2c092993c6
DESKTOP-SVA1RBJ\leewoo <dlguddn3214@naver.com>
2018-08-10 15:38:34 +0900

    no message

---
+++ study/180809todoAppSlotPropsOnEmit/slot/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <modal v-if="onOff">
+      <p slot="header">헤더</p>
+      <p slot="bottom">바텀</p>
+    </modal>
+    <button @click="onf()">버튼</button>
+  </div>
+</template>
+
+<script>
+import modal from './modal'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      onOff:false
+    }
+  },
+  methods:{
+    onf(){
+     
+      this.onOff = !this.onOff;
+    }
+  },
+  components:{
+    modal
+  }
+}
+</script>
+
+<style> 
+</style>
---
+++ study/180809todoAppSlotPropsOnEmit/slot/src/modal.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <slot name="header">이것은 슬롯의 디폴트 헤더</slot>
+        <slot name="middle">이것은 슬롯의 디폴트 바텀</slot>
+        <slot name="bottom">이것은 슬롯의 디폴트 바텀</slot>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name:'modal'
+    }
+</script>
